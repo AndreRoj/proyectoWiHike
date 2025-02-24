e5c548cfb84008fbfa5a6ccc7dcbf562026a49ff
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2025-02-23 23:03:23 -0400

    Rutas Populares

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { RutasPopulares } from './RutasPopulares'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <RutasPopulares
+        link="https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvOTEwODkxMjUvMDc2YzFiN2QyM2IzZGRkZDhkNjk3YThmZGRhZTdlZWMuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ=="
+        nombre="Sabas Nieves"
+        nombreGuia="Jose Fernandez"
+        duracion="1hora 30minutos"
+        dificultad= "Alta"
+
+      />
       <h2>Hi Bestieeeeeeeeeeee</h2>
       <h2>se logro</h2>
     </>
---
+++ src/RutasPopulares.jsx
@@ -0,0 +1,26 @@
+import './RutasPopulares.css';
+
+
+export function RutasPopulares({ link, nombre, nombreGuia, duracion, dificultad }) {
+  return (
+    <article className="ruta-popular">
+      <div className="imagen-container">
+        <img src={link} alt={nombre} className="imagen" />
+        <div className="mas-popular">Más popular</div>
+        <h1 className="nombre">{nombre}</h1>
+      </div>
+      <div className="detalles">
+        <div className="guia">
+          <strong>{nombreGuia}</strong> Guía de senderismo
+        </div>
+ 
+        <div className="duracion">
+          Duración {duracion}
+        </div>
+        <div className="dificultad">
+          Dificultad {dificultad}
+        </div>
+      </div>
+    </article>
+  );
+}
