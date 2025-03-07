a0c1eda68505a1932f9be0ac43fe3a3bd56f189c
salomegarcia0 <salome.garcia@correo.unimet.edu.ve>
2025-03-07 18:24:47 -0400

    intento #1 de arreglar pagina en blanco

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import NotFound from './pages/NotFound';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
