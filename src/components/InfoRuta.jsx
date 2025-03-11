af6c9cfadc9a3fad4038f34e6654dd3c773a0d06
AndreRoj <130945779+AndreRoj@users.noreply.github.com>
2025-03-10 22:31:20 -0400

    Componente de InfoRuta con informacion e imagenes

--- src/components/InfoRuta.jsx
+++ src/components/InfoRuta.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/InfoRuta.css"
 import { FaStar } from "react-icons/fa";
 
-export function InfoRuta({nombre, estrellas, imagenPrincipal, imagen2, imagen3}) {
+export function InfoRuta({nombre, estrellas, imagenPrincipal, imagen2, imagen3, descripcion, distancia, desnivel_positivo, horas, minutos}) {
     return (
         <div className="rutaInfo-content" syte>
                 <h1 className="nombreRuta">{nombre}</h1>
@@ -19,7 +19,30 @@ export function InfoRuta({nombre, estrellas, imagenPrincipal, imagen2, imagen3})
                     <img src={imagen3} alt="Imagen lateral 2" />
                 </div>
             </div>
-            
+            <p className="infoRuta-description">
+                {descripcion}
+            </p>
+            <div className="metricas-container">
+                <div className="metrica">
+                    <span className="valor">{distancia}</span>
+                    <span className="unidad">km</span>
+                    <span className="detalles">Distancia</span>
+                </div>
+                <div className="separador"></div>
+                <div className="metrica">
+                    <span className="valor">{desnivel_positivo}</span>
+                    <span className="unidad">m</span>
+                    <span className="detalles">Desnivel Positivo</span>
+                </div>
+                <div className="separador"></div>
+                <div className="metrica">
+                    <span className="valor">{horas}</span>
+                    <span className="unidad">h</span>
+                    <span className="valor">{minutos}</span>
+                    <span className="unidad">min</span>
+                    <span className="detalles">Tiempo Estimado</span>
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/InfoRutas.jsx
+++ src/pages/InfoRutas.jsx
@@ -12,6 +12,12 @@ export default function InfoRutas() {
             imagenPrincipal="https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNTYxNzU0NzIvZGYzZWZiNmIwZDRmOTZhMDQ5ZDExZGM0NjYyYThmZWUuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ=="
             imagen2="https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvOTEwODkxMjYvM2YxNGZiODUxNGI2YmJjZTA3YzkxNGM3ZTlhMTI2MGIuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ=="
             imagen3="https://images.alltrails.com/eyJidWNrZXQiOiJhc3NldHMuYWxsdHJhaWxzLmNvbSIsImtleSI6InVwbG9hZHMvcGhvdG8vaW1hZ2UvNzc4ODI2ODgvODAwNTE5MjIxZmU0YTc5NDIxMjVlZjZmNDIyY2RmNmUuanBnIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJ3ZWJwIiwicmVzaXplIjp7IndpZHRoIjoyMDQ4LCJoZWlnaHQiOjIwNDgsImZpdCI6Imluc2lkZSJ9LCJyb3RhdGUiOm51bGwsImpwZWciOnsidHJlbGxpc1F1YW50aXNhdGlvbiI6dHJ1ZSwib3ZlcnNob290RGVyaW5naW5nIjp0cnVlLCJvcHRpbWlzZVNjYW5zIjp0cnVlLCJxdWFudGlzYXRpb25UYWJsZSI6M319fQ=="
+            descripcion="Ruta de ida y vuelta de 3,9-km cerca de Municipio Sucre, Miranda. Se considera una ruta moderada con una duracion media de 1 h 55 min. Es una región muy popular para el senderismo y pasear, por lo que es probable encontrarse con otras personas mientras se está por la zona. "
+            distancia="3,9"
+            desnivel_positivo='356'
+            // duracion dividido colocando las horas y los minutos por separado
+            horas="1"
+            minutos="55"
             />
         </div>
         
