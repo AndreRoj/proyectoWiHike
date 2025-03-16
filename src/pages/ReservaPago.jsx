dfd6ddd7f251e5887a8554928a5e542b93797499
nicole0807-glitch <nicole.tolve@correo.unimet.edu.ve>
2025-03-15 23:37:29 -0400

    pagina reservapago 2.0

--- src/pages/ReservaPago.jsx
+++ src/pages/ReservaPago.jsx
@@ -1,7 +1,51 @@
 function ReservaPago(){
     return (
         <div>
-            <h1>ReservaPago</h1>
+            <div className="Izquierda">
+            <div className="Datospersonales">
+                <span>Datos Personales</span>
+                <div className='separador'></div> {/* Separador */}
+                <div className="row1">
+                    <span>Nombre Completo</span>
+                    <span>Telefono</span>
+                    <span>Correo</span>
+                </div>
+                <div className="row2">
+                    {/*recorrer lista user y extraer datos*/}
+           </div>
+              <div className="Datosruta">
+                 <span>Informacion del recorrido</span>
+                 <div className='separador'></div> {/* Separador */}
+                 <div className="row1">
+                      <span>Ruta</span>
+                      <span>Fecha</span>
+                      <span>Hora</span>
+                      <span>Duracion</span>
+                 </div>
+                 <div className="row2">
+                      {/*recorrer lista user y extraer datos*/}
+                 </div>
+              </div>
+              <div className="FinalizarPedido">
+                    <button className="reserva">Finalizar Pedido</button>
+             </div>
+             <div className='separador'></div> {/* Separador */}
+             <div className="columna1">
+                <span>Precio:</span>
+                {/*....*/}
+             </div>
+             <div className="columna2">
+                <span>Impuestos + IVA:</span>
+                {/*....*/}
+             </div>
+             <div className='separador'></div> {/* Separador */}
+             <div className="columna3">
+                <span>Total a pagar:</span>
+                {/*....*/}
+             </div>
+
+            </div>
+           </div>
         </div>
     )
 }
