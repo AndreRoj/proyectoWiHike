93f7f5f63a819e5a219250cfac8d469b9d70cb9b
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2025-03-24 21:54:15 -0400

    .

--- src/components/InfoRuta.jsx
+++ src/components/InfoRuta.jsx
@@ -137,7 +137,7 @@ export function InfoRuta({ id, nombre, estrellas, imagen, imagen2, imagen3, desc
     return (
         <div className="rutaInfo-content">
             <div className="nombreRuta">{nombre}</div>
-            <div className="gallery">
+            <div className="InfoRutagallery">
                 <div className="main-imag">
                     <img src={imagen} alt="Imagen principal" />
                 </div>
