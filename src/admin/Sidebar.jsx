ec7f337d4ebc0c34e8e1c81614f633360c4b00c2
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2025-03-25 00:54:49 -0400

    Update Sidebar.jsx

--- src/admin/Sidebar.jsx
+++ src/admin/Sidebar.jsx
@@ -1,9 +1,57 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import "./Sidebar.css"; 
+
+
+
+// const handleLogout = async () => {
+//     try {
+//       await signOut(auth); // Cierra la sesión del usuario
+//       console.log("Usuario cerró sesión");
+//       navigate('/'); // Redirige a la página principal
+//     } catch (error) {
+      
+//       console.error("Error al cerrar sesión:", error);
+//     }
+//   };
+
+// function Sidebar() {
+//   return (
+//     <div className="sidebar">
+//       <ul className="sidebar-list">
+//         <li className="sidebar-item">
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/adminrutas" className="sidebar-link">
+//             Rutas
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/usuarios" className="sidebar-link">
+//             Solicitudes
+//           </Link>
+//         </li>
+//         <li className="options" onClick={handleLogout} style={{ color: 'red' }}>Salir</li>
+  
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
+import  { useState, useEffect } from 'react';
+
+
+
+import { UserContext } from '../Context/UserContext';
+import { app } from '../firebase';
+import { getAuth, signOut } from 'firebase/auth';
 
 
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase'; // Asegúrate de que esta ruta sea correcta
+
 import "./Sidebar.css";
 
 function Sidebar() {
