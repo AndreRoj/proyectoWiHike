f64ff3ec4ef682e2c0f1391a9c9bfc62aac2e826
josedaniel5092003 <jose-fernandez@correo.unimet.edu.ve>
2025-03-25 00:00:40 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
  
       <Route >
           <Route path="/adminrutas" element={<Adminrutas/>}  />
-          <Route path="/solicitudes" element={<Usuarioadmin/>} />
+          <Route path="/usuarios" element={<Usuarioadmin/>} />
           <Route path='*' element = {<NotFound/>}/>
       
 
